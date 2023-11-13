308b69570c805102b73e4b009ee5dbe850c4281f
Lev1ossa <ikehhui@gmail.com>
2023-11-13 06:12:57 +0300

    feat: add 404 page tests & add checking test in precommit

--- src/components/App/router/router.tsx
+++ src/components/App/router/router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import { MainPage } from '../../../Pages/Main/MainPage';
 import { ErrorBoundary } from '../../ErrorBoundary/ErrorBoundary';
 import { ProductsDetailed } from '../../Products/ProductDetailed/ProductDetailed';
+import { NotFoundPage } from '../../../Pages/NotFound/NotFoundPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
+    errorElement: <NotFoundPage />,
     children: [
       {
         path: '',
